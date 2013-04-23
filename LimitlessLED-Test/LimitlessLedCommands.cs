7641294de95e7904c9f07bc1a8b392aa22e2d07e
glen <glenbalks@gmail.com>
2013-04-24 00:04:47 +1200

    Rearranged everything, added more things that can happen
    It's all command line switches now.   I must however sleep.. so I'll
    look at this again later.

--- LimitlessLED-Test/LimitlessLedCommands.cs
+++ LimitlessLED-Test/LimitlessLedCommands.cs
@@ -6,7 +6,14 @@
     /// </summary>
     class LimitlessLedCommands
     {
-        public static byte[] Group1AllOn  = new byte[] { 0x38, 0x0, 0x55 };
-        public static byte[] Group1AllOff = new byte[] { 0x3B, 0x0, 0x55 };
+        public static byte[] AllOn          = new byte[] { 0x35, 0x0, 0x55 };
+        public static byte[] AllOff         = new byte[] { 0x39, 0x0, 0x55 };
+        public static byte[] AllNightMode   = new byte[] { 0xB9, 0x0, 0x55 };
+        
+        public static byte[] BrightnessUp   = new byte[] { 0x3C, 0x0, 0x55 };
+        public static byte[] BrightnessDown = new byte[] { 0x34, 0x0, 0x55 };
+
+        public static byte[] Group1AllOn    = new byte[] { 0x38, 0x0, 0x55 };
+        public static byte[] Group1AllOff   = new byte[] { 0x3B, 0x0, 0x55 };
     }
 }
--- LimitlessLED-Test/Program.cs
+++ LimitlessLED-Test/Program.cs
@@ -15,50 +15,107 @@ namespace LimitlessLED_Test
             
             //initialize variables
             string bridgeipAddress = ConfigurationManager.AppSettings["ip"];
-                    
-            //Process Command Line Arguements
-
-            if (args.Length == 0)
-            {
-                Console.WriteLine("There are hidden command line stuffs");
-            }
-
-            else if (args[0] == "flash")
-            {
-                Console.WriteLine("Flash, ahhhaAHHHHAaaahhh");
-            }
-            else if (args[0] == "blink")
-            {
-                Console.WriteLine("Not as cool as flash");
-            }
-    
 
             //Connect to LimitlessLED Wifi Bridge Receiver
             System.Net.Sockets.UdpClient udpClient = new System.Net.Sockets.UdpClient(bridgeipAddress, 50000);
-        
-            
-        /*  Removed to allow testing of strobe mode below
-            //Send hex command 38 which is "Turn Group1 LED lights ON" yes it remembers the last brightness and color, each LED contains a memory chip.
-            udpClient.Send(new byte[] {
-                                             0x38,
-                                             0x0,
-                                             0x55
-                                             }, 3);
-            //ToDo: send as many different commands here as you like, just change the number above where you see &H38
 
-            //Close Connection
-            udpClient.Close();
-        */
-     
-    
-    //Strobe mode:  Flash the lights till a key is pressed
-     while (!Console.KeyAvailable)
-            {
-                    udpClient.Send(LimitlessLedCommands.Group1AllOff, 3);
-                    System.Threading.Thread.Sleep(100);
-                    udpClient.Send(LimitlessLedCommands.Group1AllOn, 3);
-                    System.Threading.Thread.Sleep(100);
-             }
+            //Process Command Line Arguements
+
+                        if (args.Length != 0)
+                        {
+                            switch (args[0])
+                            {
+                                case "flash":
+                                case "strobe":
+                                case "blink":
+                                    {
+                                        //Strobe mode:  Flash the lights till a key is pressed 
+                                            Console.WriteLine("Flash, ahhhaAHHHHAaaahhh");                        
+                                            while (!Console.KeyAvailable)
+                                             {
+                                                udpClient.Send(LimitlessLedCommands.Group1AllOff, 3);
+                                                System.Threading.Thread.Sleep(100);
+                                                udpClient.Send(LimitlessLedCommands.Group1AllOn, 3);
+                                                System.Threading.Thread.Sleep(100);
+                                            }
+                                            return;
+                                    }
+                                case "fadeDown":
+                                case "fadedown":
+                                case      "dim":
+                                    {
+                                        //Fade  group 1 down
+                                        Console.WriteLine("Not as cool as flash");
+                                        //ensure group one is on, and selected by the wifi bridge
+                                        udpClient.Send(LimitlessLedCommands.Group1AllOn, 3);
+                                        System.Threading.Thread.Sleep(150);
+                                        //Fade selected group down 10 steps  
+                                        for (int i = 1; i < 10; i++)
+                                        {
+                                            Console.WriteLine("This is dimming level " + i);
+                                            udpClient.Send(LimitlessLedCommands.BrightnessDown, 3);
+                                            System.Threading.Thread.Sleep(1000);
+
+                                        }
+                                        return;
+                                    }
+
+                                case "fadeUp":
+                                case "fadeup":
+                                case "brighten":
+                                    {
+                                     //Fade  group 1 up
+                                        Console.WriteLine("Not as cool as flash");
+                                        //ensure group one is on, and selected by the wifi bridge
+                                        udpClient.Send(LimitlessLedCommands.Group1AllOn, 3);
+                                        System.Threading.Thread.Sleep(150);
+                                        //Fade selected group up 10 steps  
+                                        for (int i = 1; i < 10; i++)
+                                        {
+                                            Console.WriteLine("This is dimming level " + i);
+                                            udpClient.Send(LimitlessLedCommands.BrightnessUp, 3);
+                                            System.Threading.Thread.Sleep(1000);
+
+                                        }
+                                        return;
+                                    }
+                                case "allOff":
+                                case "alloff":
+                                    {
+                                       //Turn off all the lights
+                                        udpClient.Send(LimitlessLedCommands.AllOff, 3);
+                                        return;
+                                    }
+                                case "allOn":
+                                case "allon":
+                                    {
+                                        //Turn off all the lights
+                                        udpClient.Send(LimitlessLedCommands.AllOn, 3);
+                                        return;
+                                    }
+                                case "allNightMode":
+                                case "allnightmode":
+                                    {
+                                        //switch all white lights to low power mode
+                                        udpClient.Send(LimitlessLedCommands.AllOff, 3);
+                                        System.Threading.Thread.Sleep(101);  //aparently 100ms isn't enough, I need 101 
+                                        udpClient.Send(LimitlessLedCommands.AllNightMode,3);
+                                        return;
+                                    }
+
+                                default:
+                                    {   //the the user we have no idea WTF they wanted
+                                        Console.WriteLine("I have no idea what you wanted me to do");
+                                        return;
+                                    }
+                            }
+                        }
+                        else
+                        {
+                        Console.WriteLine("There are input arguments required.  I really should put something here to explain them for you");
+                        }
+
+  
 
         }
     }
